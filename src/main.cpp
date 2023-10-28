356a5c73151c8af71171ec5d4c7facddfed3ca6b
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-10-28 00:03:52 -0700

    added skeleton code for main routine as a comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,26 @@
  * @version 1.0.2
  * @date 2023/10/27
  *
- * GameScreenLinkedList class definition.
+ * The main routine. Contains the game loop.
  *
  */
 
 #include "../include/gamescreenlinkedlist.h"
+
+int main()
+{
+  /*
+  GameScreenLinkedList gameSequence; (we create a GameScreenLinkedList)
+  GameScreenLinkedList* current; (set it to the head -- the load screen)
+  load(input.txt, current)
+  while ('the player hasn't quit the program') {
+    display(current); (hopefully the player is at the main menu screen)
+    std::string playerIn;
+    std::cin >> playerIn;
+    gameSequence.clear(current);
+    GameScreenLinkedList* current = gameSequence.search(playerIn); (the next game screen is determined by the player's input.
+  }
+  */
+  
+  return 0;
+}
