a8a1ea03dd21a9548e4960de380b2b03d640f9d6
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-10-27 23:44:07 -0700

    Created utility.cpp
    currently blank

---
+++ src/utility.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file utility.cpp
+ * @author Trevor
+ * @author Austin
+ * @author Robert
+ * @author Dustin
+ * @author Lucas
+ * @version 1.0.2
+ * @date 2023/10/27
+ *
+ * Contains implementations of functions in utility.h.
+ *
+ */
+
+#include "../include/utility.h"
+
+// function implementations go here
