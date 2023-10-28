d17afadfa83b49bc350101655573d96327e7432e
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-10-27 23:34:11 -0700

    Created utility.h

---
+++ include/utility.h
@@ -0,0 +1,18 @@
+/**
+ * @file utility.h
+ * @author Trevor
+ * @author Austin
+ * @author Robert
+ * @author Dustin
+ * @author Lucas
+ * @version 1.0.2
+ * @date 2023/10/27
+ *
+ * includes libraries and any functions not tied to a class (functions that should be usable by all files in the program)
+ *
+ */
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
