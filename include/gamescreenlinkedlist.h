562a10a06c04bf497404499cb72ab97c3442811a
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-11-03 16:02:44 -0700

    changed instance of 'root' to 'head'

--- include/gamescreenlinkedlist.h
+++ include/gamescreenlinkedlist.h
@@ -58,7 +58,7 @@ class GameScreenLinkedList {
 		//Displays the screen text in node parameter, along with the option texts, and the text blurbs/descriptions for each option
 
 
-		GameScreenLinkedList* search(const std::string screenID, const GameScreenLinkedList* root) const;
+		GameScreenLinkedList* search(const std::string screenID, const GameScreenLinkedList* head) const;
 		//Loop through the list and campare the screenID of each node with parameter 'screenID'
 		//If a node matches, return it.
 		//Otherwise, return NULL.
