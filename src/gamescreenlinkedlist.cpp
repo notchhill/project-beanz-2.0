eb0061462ad3671fe16df5e42799eafbd967d122
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-11-07 13:02:37 -0800

    made display method not look horrible

--- src/gamescreenlinkedlist.cpp
+++ src/gamescreenlinkedlist.cpp
@@ -21,18 +21,33 @@
 @return void
 */
 void GameScreenLinkedList::display(GameScreenLinkedList* node) {
-	std::cout << node->screenText << std::endl;
-	std::cout << node->option1.optionTextBlurb << " -- [";
-	std::cout << node->option1.optionChoiceText << "]" << std::endl;
-	std::cout << node->option2.optionTextBlurb << " -- [";
-	std::cout << node->option2.optionChoiceText << "]" << std::endl;
-	std::cout << node->option3.optionTextBlurb << " -- [";
-	std::cout << node->option3.optionChoiceText << "]" << std::endl;
-	std::cout << node->option4.optionTextBlurb << " -- [";
-	std::cout << node->option4.optionChoiceText << "]" << std::endl;
-	std::cout << node->option5.optionTextBlurb << " -- [";
-   	std::cout << node->option5.optionChoiceText << "]" << std::endl;
-	std::cout << std::endl << std::endl << std::endl;
+	std::cout << node->screenText << std::endl << std::endl;
+	if (node->option1.optionTextBlurb == "")
+	{
+		std::cout << node->option1.optionTextBlurb << " -- [";
+		std::cout << node->option1.optionChoiceText << "]" << std::endl;
+	}
+	if (node->option1.optionTextBlurb == "")
+	{
+		std::cout << node->option2.optionTextBlurb << " -- [";
+		std::cout << node->option2.optionChoiceText << "]" << std::endl;
+	}
+	if (node->option1.optionTextBlurb == "")
+	{
+		std::cout << node->option3.optionTextBlurb << " -- [";
+		std::cout << node->option3.optionChoiceText << "]" << std::endl;
+	}
+	if (node->option1.optionTextBlurb == "")
+	{
+		std::cout << node->option4.optionTextBlurb << " -- [";
+		std::cout << node->option4.optionChoiceText << "]" << std::endl;
+	}
+	if (node->option1.optionTextBlurb == "")
+	{
+		std::cout << node->option5.optionTextBlurb << " -- [";
+		std::cout << node->option5.optionChoiceText << "]" << std::endl;
+	}
+	std::cout << std::endl << std::endl;
 }
 
 /*
