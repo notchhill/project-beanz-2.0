2ecc8a2563119617cd962170786fdd19dbb261ff
Laxxius <132426444+Laxxius1@users.noreply.github.com>
2023-11-08 12:03:17 -0800

    added definitions for getPlayerIn and nicePrint

--- include/utility.h
+++ include/utility.h
@@ -17,3 +17,12 @@
 #include <fstream>
 #include <string>
 #include <stdlib.h>
+
+// gets a string from the user, and copies it into another string (initially empty), character-by-character, based on several conditions
+//    if the next character is a lowercase letter, it gets concatenated onto the second string
+//    if the next character is an uppercase letter, it gets converted into a lowercase letter, and then concatenated onto the second string
+//    if the next character is a symbol, the character is not concatenated onto the second string
+std::string getPlayerIn();
+
+// this function will be passed a string. It will print the string character-by-character, and insert a newline every 10th ' ' character.
+void nicePrint(std::string text);
