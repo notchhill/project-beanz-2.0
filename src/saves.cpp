4ec3eb299b64be6b29fa23650475bab9401fef9a
4treess <trevordeclark04@gmail.com>
2023-12-07 21:55:28 -0800

    Actually pushing saves

---
+++ src/saves.cpp
@@ -0,0 +1,93 @@
+#include "../include/saves.h"
+
+Saves::Saves(){
+    this->autosaveScreenID = "";
+    this->userSaveScreenID = "";
+    this->userSaveHP = 100;
+    this->autosaveHP = 100;
+    this->saveFile = "";
+}
+
+Saves::Saves(std::string fileName){
+        std::ifstream file;
+        file.open(fileName);
+
+        if(!file.fail()){
+            std::getline(file, this->autosaveScreenID);
+            if (this->autosaveScreenID.back() == '\r') 
+			{this->autosaveScreenID.pop_back();}
+
+            std::getline(file, this->userSaveScreenID);
+            if (this->userSaveScreenID.back() == '\r') 
+			{this->userSaveScreenID.pop_back();}
+
+            file >> this->autosaveHP;
+            file >> this->userSaveHP;
+            std::string garbage;
+            std::getline(file, garbage);
+        }
+			this->saveFile = fileName;
+
+    		file.close();
+}
+
+Saves::~Saves(){
+    //trunc is to discard old file, and create a new one.
+    std::ofstream file (this->saveFile, std::ofstream::trunc);
+    file << this->autosaveScreenID << "\n";
+    file << this->userSaveScreenID << "\n";
+    file << this->autosaveHP << "\n";
+    file << this->userSaveHP;
+    file.close();
+}
+
+void Saves::save(const GameScreenLinkedList* current, GameScreenLinkedList* head, Player* beanzGuy){
+    this->userSaveScreenID = current->screenID;
+    this->userSaveHP = beanzGuy->get_hp();
+    head->option3.optionscreenID = current->screenID;
+    head->option3.optionTextBlurb = "Load Previous Manual Save";
+	head->option3.optionChoiceText = "load";
+}
+
+void Saves::autosave(GameScreenLinkedList* prev, GameScreenLinkedList* current, GameScreenLinkedList* head, Player* beanzGuy){
+	if(current != NULL && current->screenID != "LS00400"){
+        GameScreenLinkedList* ptr;
+        if(current->option1.optionChoiceText == "restart" && prev != NULL){
+            ptr = prev;
+        }else{
+            ptr = current;
+        }
+
+        this->autosaveScreenID = ptr->screenID;
+        this->autosaveHP = beanzGuy->get_hp();
+        head->option3.optionscreenID = current->screenID;
+        head->option2.optionTextBlurb = "Load Previous Auto Save";
+	    head->option2.optionChoiceText = "autosave";
+    }
+}
+
+
+ //Returns autosaveScreenID
+std::string Saves::getAutosaveScreenID(){
+    return this->autosaveScreenID;
+}
+
+//Returns userSaveScreenID
+std::string Saves::getUserSaveScreenID(){
+    return this->userSaveScreenID; 
+}
+
+//Returns autosaveHP
+int Saves::getAutosaveHP(){
+    return autosaveHP;
+}
+
+//Returns userSaveHP
+int Saves::getUserSaveHP(){
+    return this->userSaveHP;
+}
+
+//Returns the directory of the savefile
+std::string Saves::getSaveFile(){
+    return this->saveFile;
+}
